119cc08e40f99b70fce2f92d747b7cb20a652099
TJ <philosophy.cheer@gmail.com>
2019-03-03 17:08:41 -0500

    Updates

---
+++ precache-manifest.098c186919c9ee1df6af6531a59259c8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/githubcard/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "968d30ce69c26e7ae84b",
+    "url": "/githubcard/static/css/main.649a6be2.chunk.css"
+  },
+  {
+    "revision": "8c8b70c8ba01cc28ff89",
+    "url": "/githubcard/static/js/runtime~main.8c8b70c8.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/githubcard/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "fa315ae2114d91187047",
+    "url": "/githubcard/static/js/2.fa315ae2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubcard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/githubcard/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/githubcard/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/githubcard/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/githubcard/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/githubcard/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "968d30ce69c26e7ae84b",
+    "url": "/githubcard/static/js/main.968d30ce.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/githubcard/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/githubcard/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/githubcard/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/githubcard/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/githubcard/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/githubcard/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/githubcard/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/githubcard/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/githubcard/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "fa315ae2114d91187047",
+    "url": "/githubcard/static/css/2.652a8865.chunk.css"
+  },
+  {
+    "revision": "fb19c31cd0322366a0e8eef6334fe5ee",
+    "url": "/githubcard/index.html"
+  }
+];
